e83c7591e34eacf7d8aabc1513153bf567eb96cf
Wairimu <charitykabuu21@gmail.com>
2023-07-28 17:04:18 +0300

    Update 0-print.py

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,2 +1,6 @@
-#Filename: 0-print.py
+# Filename: 0-print.py
+
 print("Programming is like building a multilingual puzzle")
+
+# Add an input prompt to keep the terminal open after execution
+input("Press Enter to exit...")
