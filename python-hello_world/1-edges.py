dd75e26a90e3f99d245f430be5e08af5b1ec2292
Wairimu <charitykabuu21@gmail.com>
2023-08-02 15:05:18 +0300

    Create 1-edges.py

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,8 @@
+#Filename: 1-edges.py
+
+#!/usr/bin/python3
+word = "Holberton"
+
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
