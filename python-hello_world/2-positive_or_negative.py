9f191397b4a5669c0210d1d9b803904ab361595e
Wairimu <charitykabuu21@gmail.com>
2023-08-02 15:46:26 +0300

    Update 2-positive_or_negative.py

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -7,11 +7,4 @@ elif number == 0:
   return "is zero"
 else:
   return "is negative"
-   
-if __name__ == "__main__":
-   for arg in sys.argv[1:]:
-      try:
-         number = int(arg)
-         print(f"{number} {check_number(number)}")
-      except ValueError:
-         ptint(f"Invalid input: {arg} is not a valid interger.")
+
