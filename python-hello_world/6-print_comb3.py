db8b8fcbe6292742075bb227b5c0d7f5681eddea
Wairimu <charitykabuu21@gmail.com>
2023-08-02 22:00:06 +0300

    Update 6-print_comb3.py

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,1 @@
-for i in range(1, 10):
-    for j in range(i + 1, 10):
-        print(f"{i:0d}{j:0d}", end=", ")
-print("\b\b")
+print(", ".join(f"{i}{j}" for i in range(10) for j in range(i + 1, 10)))
