f3369cc2a2b71d0b47756000b70507bcc52122f3
Wairimu <charitykabuu21@gmail.com>
2023-08-02 21:35:22 +0300

    Update 5-print_comb2.py

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 for num in range(100):
-    print(f"{num:02d}", end=(", " if num < 99 else "\n"))
+    print(f"{num:02d}", end=(", " if num < 99 )
+                             else 
+                             print ("\n")
