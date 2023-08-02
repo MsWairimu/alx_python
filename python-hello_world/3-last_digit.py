9ba07d9f55231dc5d3d6b3339728349e3046a53a
Wairimu <charitykabuu21@gmail.com>
2023-08-02 16:20:37 +0300

    Update 3-last_digit.py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-import random 
-number = random.randint(-10000, 10000)
+import random
 
 def check_number_last_digit(number):
     last_digit = abs(number) % 10
@@ -10,17 +9,14 @@ def check_number_last_digit(number):
     elif last_digit == 0:
         return "and is 0"
     else:
-        return f"is less than 6 and not 0"
+        return "and is less than 6 and not 0"
 
+number = random.randint(-10000, 10000)
 number_str = str(number)
 
 last_digit_of_string = number_str[-1]
 number_part = number_str[1:-1]
 last_digit_of_number = abs(number) % 10
 
-output = f"-{last_digit_of_string}, followed by\n" \
-         f"-{number_part}, followed by\n" \
-         f"-the string is, followed by\n" \
-         f"-the last digit of number, {last_digit_of_number}, {check_number_last_digit(number)}"
-
+output = f"Last digit of {number} is {last_digit_of_number} {check_number_last_digit(number)}"
 print(output)
