5deb09f0345f188c82989f1546907c06d267ecf8
Wairimu <charitykabuu21@gmail.com>
2023-08-02 15:23:01 +0300

    Create 2-positive_or_negative.py

---
+++ python-hello_world/2-positive_or_negative.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import random
+number = random.randit(-10, 10)
+number = 10
+if the number is greater that 0: print("is positive")
+if the number is 0: print("is zero")
+if the number is less than 0: print("is negative")
