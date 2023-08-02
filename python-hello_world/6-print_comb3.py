6022da3573b36a26ca1bb65c52a1a08bec70baa0
Wairimu <charitykabuu21@gmail.com>
2023-08-02 22:31:58 +0300

    Update 6-print_comb3.py

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,8 @@
-for i in range(1, 10):
+#!/usr/bin/python3
+for i in range(10):
     for j in range(i + 1, 10):
-        print(f"{i}{j}", end=", ")
-print("\b\b")
+        if (i != 8 or j != 9):
+            end_char = ", "
+        elif(i == 8 and j == 9):
+            end_char = "\n"
+        print("{:d}{:d}".format(i,j), end=end_char)
