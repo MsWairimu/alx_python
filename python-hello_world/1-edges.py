780b4cb292ba0739a4c1e2c5d26e606a8ffff44c
Wairimu <charitykabuu21@gmail.com>
2023-08-02 15:13:32 +0300

    Update 1-edges.py

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,17 +1,8 @@
-#Filename: 1-edges.py
-
 #!/usr/bin/python3
 word = "Holberton"
-
-# Extract the first 3 letters
 word_first_3 = word[:3]
-
-# Extract the last 2 letters
 word_last_2 = word[-2:]
-
-# Extract the middle word (excluding the first and last letter)
 middle_word = word[1:-1]
-
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
