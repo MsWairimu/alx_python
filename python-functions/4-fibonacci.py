a9c827fb6c118456d87c5a4153389da43bc4836b
Wairimu <charitykabuu21@gmail.com>
2023-08-04 13:18:47 +0300

    Update 4-fibonacci.py

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,1 +1,20 @@
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+
+    if n == 1:
+        return [0]
+
+    fib_seq = [0, 1]
+
+    while len(fib_seq) < n:
+        next_num = fib_seq[-1] + fib_seq[-2]
+        fib_seq.append(next_num)
+
+    return fib_seq
+
+# Test the function
+n = 10
+result = fibonacci_sequence(n)
+print(result)  # Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 
