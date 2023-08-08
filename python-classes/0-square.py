71aa92e64273d80c5fc5ed0ff5488984943c4c5c
Wairimu <charitykabuu21@gmail.com>
2023-08-08 16:03:40 +0300

    Update 0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,27 @@
+class Square:
+    """ This class defines a square by its size. """
+
+    def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
+
+# Test the Square class
+if __name__ == "__main__":
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+
+    try:
+        print(my_square.size)
+    except AttributeError as e:
+        print(e)
+
+    try:
+        print(my_square.__size)
+    except AttributeError as e:
+        print(e)
