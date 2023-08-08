a63fc3b70929be8809cade48c8f36fef09c9e729
Wairimu <charitykabuu21@gmail.com>
2023-08-08 11:48:10 +0300

    Update 2-multiple_returns.py

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,1 +1,15 @@
+def multiple_returns(sentence):
 
+  result = ()
+  length = len(sentence)
+
+  if length >= 1:
+    first = sentence[0]
+    result = (length, first)
+    return result 
+
+else:
+   first = None
+   length = 0
+   result = (length, first)
+   return result
