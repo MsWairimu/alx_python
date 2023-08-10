898a9f5197ddac56b537a6f7edb8c4541952ece8
Wairimu <charitykabuu21@gmail.com>
2023-08-10 09:32:48 +0300

    Update 4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,23 @@
+#!/usr/bin/python3
+"""
+A class for checking if an object is an instance of the specified class.
+"""
 
+
+class TypeChecker:
+    """
+    A class for checking if an object is an instance of the specified class.
+    """
+
+    def is_same_class(self, obj, a_class):
+        """
+        Check if the object is exactly an instance of the specified class.
+
+        Args:
+            obj: The object to check.
+            a_class: The class to compare the object with.
+
+        Returns:
+            bool: True if the object is an instance of the specified class, False otherwise.
+        """
+        return type(obj) is a_class
