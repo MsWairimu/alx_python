8bfbececedd4f34b9e4a5e2bd13893a4d051bd48
Wairimu <charitykabuu21@gmail.com>
2023-08-10 09:38:10 +0300

    Update 4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,25 +1,36 @@
+#!/usr/bin/python3
 class Square:
     def __init__(self, size=0):
         self.size = size
 
     @property
     def size(self):
-        return self.__size
+        return self._size
 
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        self._size = value
 
     def area(self):
-        return self.__size ** 2
+        return self.size ** 2
 
     def my_print(self):
-        if self.__size == 0:
+        if self.size == 0:
             print()
         else:
-            for _ in range(self.__size):
-                print("#" * self.__size)
+            for i in range(self.size):
+                for j in range(self.size):
+                    print("#", end="")
+                print()
+    __doc__ = """
+    this is doc for class
+    """
+
+
+__doc__ = """
+this is doc for class
+"""
