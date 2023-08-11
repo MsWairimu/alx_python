f8b0de2e97ec672674480b6c8fa7e8a6f3b6c2f2
Wairimu <charitykabuu21@gmail.com>
2023-08-11 13:25:04 +0300

    Update 4-base_geometry.py

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,10 +1,40 @@
-#!/usr/bin/python3
-"""Empty BaseGeometry class"""
+"""
+This is module for class Basegeometry.
+"""
+class BaseMetaClass(type):
+    """
+    This is a BaseMetaClass
+    """
+    
+    
+    def __dir__(cls):
+        """
+        This function removes the __init_subclass (method) from the default method inherited from the parent class
+        """
+        return [attribute for attribute in super().__dir__() if attribute != "__init_subclass__"]
 
+class BaseGeometry(metaclass=BaseMetaClass):
+        """
+        This is class BaseGeometry.
+        """
+        
+        def __dir__(cls):
+            """
+            This function removes the __init_subclass (method) from the default method inherited from the parent class
+            """
+            return [attribute for attribute in super().__dir__() if attribute != "__init_subclass__"]
 
 class BaseGeometry:
-    """Class Geometry"""
+        """
+        This is class BaseGeometry.
+        """
 
-    def area(self):
-        """Raises Exception only"""
-        raise Exception('area() is not implemented')
+        def area(self):
+            raise Exception ("area() is not implemented")
+        
+        def __dir__(cls):
+            
+            """
+            This function removes the __init_subclass (method) from the default method inherited from the parent class
+            """
+            return [attribute for attribute in super().__dir__() if attribute != "__init_subclass__"]
