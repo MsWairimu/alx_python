8630b50f54db8bc330cd392b5f3020c0e6bcac14
Wairimu <charitykabuu21@gmail.com>
2023-08-11 12:06:21 +0300

    Create 4-base_geometry.py

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,9 @@
+"""Write a class BaseGeometry (based on 3-base_geometry.py)."""
+class BaseGeometry:
+  """A class representing a geometric shape.
+
+   Raises:
+            Exception: This method is not implemented in this base class.
+
+        """
+        raise Exception("area() is not implemented")
