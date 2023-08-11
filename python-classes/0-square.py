8d172554964cb997d4631758e6110098d5652ad9
Wairimu <charitykabuu21@gmail.com>
2023-08-11 22:16:34 +0300

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,1 +1,16 @@
+#!/usr/bin/python3
+"""
+module with a class 
+
+"""
+
+class Square:
+    """
+adds up
+
+"""
+    def __init__(self, size):
+        self.__size = size
+    """ private attribute"""
+
 
