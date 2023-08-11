4c5ce088459d553b74da77a66ed877eeeda81889
Wairimu <charitykabuu21@gmail.com>
2023-08-11 13:39:55 +0300

    Update 7-rectangle.py

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,20 +1,74 @@
-"""This module improves the class by raising an exception"""
+"""
+This is module for class Basegeometry.
+"""
+class BaseMetaClass(type):
+    """
+    This is a BaseMetaClass
+    """
+    
+    
+    def __dir__(cls):
+        """
+        This function removes the __init_subclass (method) from the default method inherited from the parent class
+        """
+        return [attribute for attribute in super().__dir__() if attribute != "__init_subclass__"]
 
-BaseGeometry = __import__('6-rectangle').Rectangle
+class BaseGeometry(metaclass=BaseMetaClass):
+        """
+        This is class BaseGeometry.
+        """
+        
+        def __dir__(cls):
+            """
+            This function removes the __init_subclass (method) from the default method inherited from the parent class
+            """
+            return [attribute for attribute in super().__dir__() if attribute != "__init_subclass__"]
 
+class BaseGeometry:
+        """
+        This is class BaseGeometry.
+        """
+        
+        def area(self):
+            raise Exception ("area() is not implemented")
 
+        def integer_validator(self, name, value):
+            self.name = name
+            if type(value) is not int:
+                raise TypeError (f"{name} must be an integer")
+            if value <= 0:
+                raise ValueError (f"{name} must be greater than 0")
+
+        def __dir__(cls) -> None:
+             attributes = super().__dir__()
+             return [x for x in attributes if x != '__init_subclass__']
+                
 class Rectangle(BaseGeometry):
-    """class that inherits BaseGeometry"""
-
+    """
+    This is a class Rectangle
+    """
+    
     def __init__(self, width, height):
-        """instantiation with width and height"""
-        self.__width = super().integer_validator('width', width)
-        self.__height = super().integer_validator('height', height)
+        """
+        This is an integer validator method
+        """
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
     def area(self):
-        """calculate and return the area of rectangle"""
+        """ returns the area of the object."""
         return self.__width * self.__height
 
     def __str__(self):
-        """method returns a string"""
+        """ str magic method"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
+    
+    def __dir__(cls):
+            
+            """
+            This function removes the __init_subclass (method) from the default method inherited from the parent class
+            """
+            return [attribute for attribute in super().__dir__() if attribute != "__init_subclass__"]
+    
