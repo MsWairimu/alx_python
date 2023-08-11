1d9b73b08faf61724ec0c41d167d79b79a1361b8
Wairimu <charitykabuu21@gmail.com>
2023-08-11 11:49:21 +0300

    Create 1-is_kind_of_class.py

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,11 @@
+
+""" Python Inheritance """
+
+
+def is_kind_of_class(obj, a_class):
+  """
+  Return True if the object is an instance of,
+  or if the object is an instance
+
+  """
+  return isinstance(obj, a_class)
