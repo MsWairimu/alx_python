094b3d0673cfd5b152b3f2a17b8640ccbf5b27e4
Wairimu <charitykabuu21@gmail.com>
2023-08-16 01:07:57 +0300

    Update 5-json_api.py

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,1 +1,27 @@
+#!/usr/bin/python3
+"""
+Module to send a POST request with a letter parameter to http://0.0.0.0:5000/search_user and process the response.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+
+    payload = {'q': letter}
+    response = requests.post(url, data=payload)
+
+    try:
+        data = response.json()
+        if data:
+            print("[{}] {}".format(data.get('id'), data.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
 
