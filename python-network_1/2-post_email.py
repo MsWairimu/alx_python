80909227face93be20522e901e3233938ad8a004
Wairimu <charitykabuu21@gmail.com>
2023-08-16 01:04:26 +0300

    Update 2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,1 +1,17 @@
+#!/usr/bin/python3
+"""
+Module to send a POST request with an email parameter to a given URL and display the response body.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]  # Get the URL from the command line argument
+    email = sys.argv[2]  # Get the email address from the command line argument
+
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+    content = response.text
+    print(content)
 
