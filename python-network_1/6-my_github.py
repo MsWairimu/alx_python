f84492f46c18a4bd76eb7a6e32a7430e7dcea1a7
Wairimu <charitykabuu21@gmail.com>
2023-08-16 01:09:16 +0300

    Update 6-my_github.py

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,1 +1,21 @@
+#!/usr/bin/python3
+"""
+Module to authenticate with GitHub API using Basic Authentication and display the user id.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(username, password))
+
+    try:
+        data = response.json()
+        print(data.get('id'))
+    except ValueError:
+        print("Error: Unable to retrieve user id.")
 
