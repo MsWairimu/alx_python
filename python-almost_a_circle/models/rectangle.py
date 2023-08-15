f4acd5ef46151b5ece0014ad37de505caafa8101
Wairimu <charitykabuu21@gmail.com>
2023-08-16 00:10:21 +0300

    Update rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,18 +1,12 @@
-# models/base.py
-class Base:
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
-
-# models/rectangle.py
+"""models/rectangle.py"""
 from models.base import Base
 
+"""This is the rectangle class"""
+
+
 class Rectangle(Base):
+    """calling the superclass and assigning attributes"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
@@ -20,50 +14,112 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
+    """This is the width getter"""
     @property
     def width(self):
         return self.__width
 
+    """width setter"""
     @width.setter
     def width(self, value):
+        """validation"""
         if not isinstance(value, int):
-            raise TypeError("Width must be an integer")
+            raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("Width must be a positive integer")
+            raise ValueError("width must be > 0")
         self.__width = value
 
+    """This is the height getter"""
     @property
     def height(self):
         return self.__height
 
+    """height setter"""
     @height.setter
     def height(self, value):
+        """validation"""
         if not isinstance(value, int):
-            raise TypeError("Height must be an integer")
+            raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError("Height must be a positive integer")
+            raise ValueError("height must be > 0")
         self.__height = value
 
+    """This is the x getter"""
     @property
     def x(self):
         return self.__x
 
+    """x setter"""
     @x.setter
     def x(self, value):
+        """validation"""
         if not isinstance(value, int):
-            raise TypeError("X must be an integer")
+            raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("X must be a non-negative integer")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
+    """This is the y getter"""
     @property
     def y(self):
         return self.__y
 
+    """y setter"""
     @y.setter
     def y(self, value):
+        """validation"""
         if not isinstance(value, int):
-            raise TypeError("Y must be an integer")
+            raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("Y must be a non-negative integer")
+            raise ValueError("y must be >= 0")
         self.__y = value
+    """area of the rectangle"""
+
+    def area(self):
+        """calculates the area of the rectangle"""
+        return self.__width * self.__height
+    """This is a public method"""
+
+    def display(self):
+        """It prints in stdout #"""
+        for _ in range(self.__y):
+            print()
+        for _ in range(self.__height):
+            for _ in range(self.__x):
+                print(" ", end="")
+            for _ in range(self.__width):
+                print("#", end="")
+            print()
+    """This is the str method"""
+
+    def __str__(self):
+        """"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args, **kwargs):
+        """These are the arguments
+        """
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
+
+        if kwargs:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "width" in kwargs:
+                self.width = kwargs["width"]
+            if "height" in kwargs:
+                self.height = kwargs["height"]
+            if "x" in kwargs: 
+                self.x = kwargs["x"]  
+            if "y" in kwargs: 
+                self.y = kwargs["y"]  
+
+        
