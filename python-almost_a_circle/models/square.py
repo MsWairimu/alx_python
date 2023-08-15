0e62b2c50327caedd946d994dc5dbd6b837a85f6
Wairimu <charitykabuu21@gmail.com>
2023-08-16 00:27:09 +0300

    Update square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,9 +1,18 @@
-"""This is the rectangle module import"""
+# This is the rectangle module import
 from models.rectangle import Rectangle
 
+# This is the square class inheriting from the rectangle
 class Square(Rectangle):
+    # __init method calls the function
     def __init__(self, size, x=0, y=0, id=None):
+        # calling the super class using the logic of the Rectangle
         super().__init__(size, size, x, y, id)
+        self.size = size
+    
+    # str method for printing
+    def __str__(self):
+        # defines the format for returning the square attributes
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
 
     @property
     def size(self):
@@ -11,8 +20,10 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
+        # validation
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
-
-    def __str__(self):
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
