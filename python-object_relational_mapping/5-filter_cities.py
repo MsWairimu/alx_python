55e7e3acdff77d65b36dd7c74eac1739eb7dd1f9
Wairimu <charitykabuu21@gmail.com>
2023-10-06 17:49:33 +0300

    Update 5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -20,8 +20,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=username, passwd=password, db=db_name, \
                          port=3306, host="localhost")
     cursor = db.cursor()
-    cursor.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE \
-    states.name=%s ORDER BY cities.id ASC", (state_name,))
+    cursor.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id = \
+    states.id WHERE states.name=%s ORDER BY cities.id ASC", (state_name,))
     rows = cursor.fetchall()
     print(", ".join([row[0] for row in rows]))
     cursor.close()
