9dcb9c759c704acc5fcdbc2cf41b10c0ad284016
Wairimu <charitykabuu21@gmail.com>
2023-10-06 17:27:33 +0300

    Create 3-my_safe_filter_states.py

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,38 @@
+import MySQLdb as DB
+import sys
+
+
+def states_list(username, password, db_name, state_name):
+    try:
+        dabase = DB.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=db_name,
+        )
+
+        cur = dabase.cursor()
+
+        list = "SELECT * FROM states WHERE name LIKE %s COLLATE utf8mb4_bin ORDER BY id ASC"
+
+        cur.execute(list, (state_name,))
+
+        states = cur.fetchall()
+
+        for state in states:
+            print(state)
+
+    except DB.Error:
+        print()
+    finally:
+        cur.close()
+        dabase.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+    states_list(username, password, db_name, state_name)
