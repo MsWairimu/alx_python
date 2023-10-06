ee25a51204f979149169947b5b8877d757fdda3b
Wairimu <charitykabuu21@gmail.com>
2023-10-06 16:02:56 +0300

    Update 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 """
-Script that lists all states with a name starting with n (lowercase n) from the database hbtn_0e_0_usa
+Lists all states with a name starting with n from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
