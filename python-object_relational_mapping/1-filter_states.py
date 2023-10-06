7c31fb2fb0d18536ae7af7f2bff40e2707bd3c89
Wairimu <charitykabuu21@gmail.com>
2023-10-06 15:37:10 +0300

    Update 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """
-Script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+Script that lists all states with a name starting with n (lowercase n) from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
 import sys
-import pycodestyle
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1],
@@ -14,14 +13,9 @@ if __name__ == "__main__":
                          host="localhost",
                          port=3306)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'n%' ORDER BY id ASC")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
     cursor.close()
     db.close()
-
-    # Validate code against PEP8 standards
-    style = pycodestyle.StyleGuide()
-    result = style.check_files(['script.py'])
-    print(result.total_errors)
