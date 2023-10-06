776d13a2266857b039cabd338f92ed9b5a6381aa
Wairimu <charitykabuu21@gmail.com>
2023-10-06 15:57:32 +0300

    Update 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -5,6 +5,7 @@ List all states with a name starting with n from the database hbtn_0e_0_usa
 
 import MySQLdb
 import sys
+import main_3_0
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1],
@@ -19,5 +20,5 @@ if __name__ == "__main__":
         print(row)
     cursor.close()
     db.close()
-    pip install main_3_0
+
 
